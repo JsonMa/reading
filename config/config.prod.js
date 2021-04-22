db27b804e6face43f1d41a29c55fd2d468c1f704
jsonma <mahao-0321@hotmail.com>
2021-04-22 17:28:32 +0800

    fix: resolve mongo bug

--- config/config.prod.js
+++ config/config.prod.js
@@ -4,6 +4,7 @@ exports.mongoose = {
   client: {
     url: 'mongodb://127.0.0.1/reading',
     options: {
+      useUnifiedTopology: true,
       user: 'reading',
       pass: 'reading123456',
     },
